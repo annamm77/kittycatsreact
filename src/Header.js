f6880ddb5f79e8fd692c5f5bab65a205b662c5a7
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-12 12:27:00 -0700

    added components for header, cat and footer within app

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1 className="headertitle">Seattle Shelter Cats</h1>
+        I am the header.
+      </div>
+    );
+  }
+}
+
+export default Header;
