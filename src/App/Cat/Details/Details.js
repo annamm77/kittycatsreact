eb03f50612f743da09c142e6ed061c92a8570495
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-15 13:57:34 -0700

    mock up css of fixed cat div

--- src/App/Cat/Details/Details.js
+++ src/App/Cat/Details/Details.js
@@ -5,14 +5,13 @@ class Details extends Component {
   render() {
     return (
       <div className="Details">
-        {this.props.currentCat["id"]}
         {this.props.currentCat["name"]}
         {this.props.currentCat["age"]}
         {this.props.currentCat["sex"]}
         {this.props.currentCat["breed"]}
-        {this.props.currentCat["shelterid"]}
         {this.props.currentCat["description"]}
         {this.props.currentCat["image"]}
+        
         {this.props.currentCat["address"]}
         {this.props.currentCat["city"]}
         {this.props.currentCat["state"]}
