92a2492da833b81e41805db090433770f809d069
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-16 10:09:55 -0700

    removed overall centering

--- src/App/Cat/Details/Details.js
+++ src/App/Cat/Details/Details.js
@@ -4,15 +4,21 @@ import './Details.css';
 class Details extends Component {
   render() {
     return (
-    <div className="details">
-      <p className="profilewrapper">
-        {this.props.currentCat["name"]}
-        {this.props.currentCat["age"]}
-        {this.props.currentCat["sex"]}
-        {this.props.currentCat["breed"]}
-        {this.props.currentCat["description"]}
-        {this.props.currentCat["image"]}
+    <div className="detailswrapper">
+      <img src={this.props.currentCat["image"]} alt="current cat" className="detailsimg" />
 
+      <p className="generalinfo">
+        Name: {this.props.currentCat["name"]}
+        Age: {this.props.currentCat["age"]}
+        Sex: {this.props.currentCat["sex"]}
+        Breed: {this.props.currentCat["breed"]}
+      </p>
+
+      <p className="description">
+        {this.props.currentCat["description"]}
+      </p>
+
+      <p className="contactinfo">
         {this.props.currentCat["address"]}
         {this.props.currentCat["city"]}
         {this.props.currentCat["state"]}
