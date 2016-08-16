1cc9ef52e6e3c6d52ae09b58d42463ec50f8eb77
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-16 09:18:44 -0700

    center options buttons

--- src/App/Cat/Options/Options.js
+++ src/App/Cat/Options/Options.js
@@ -5,8 +5,8 @@ class Options extends Component {
   render() {
     return (
       <div className="options">
-        <button className="btn" onClick={this.props.onNextClick}>Next</button>
         <button className="btn" onClick={this.props.onDetailsClick}>{this.props.buttonText}</button>
+        <button className="btn" onClick={this.props.onNextClick}>Next</button>
       </div>
     );
   }
